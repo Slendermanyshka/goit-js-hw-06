6b1f76bfa73d47f697b1c5dc03eec5344ec00730
Slendermanyshka <slendermanyshka@gmail.com>
2022-01-26 18:36:47 +0200

    finished task 5

---
+++ js/task-05.js
@@ -0,0 +1,10 @@
+const name_output = document.querySelector('#name-output');
+const name_input = document.querySelector('#name-input');
+
+name_input.addEventListener("input", (event) => {
+if(name_input===""){
+name_output.textContent = `Anonymous`;
+}
+else {
+    name_output.textContent = event.currentTarget.value;}
+});
