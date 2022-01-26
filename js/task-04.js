ef5ff55c8b5c545f458f745c7ee8fb518c553b47
Slendermanyshka <slendermanyshka@gmail.com>
2022-01-26 18:19:54 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -10,18 +10,3 @@ const minus = document.querySelector(`[data-action="decrement"]`);
 minus.addEventListener('click' , () => {
     counterValue-=1;
     counter.textContent=counterValue});
-/*
-let counterValue = 0; 
-
-const number = document.querySelector('#value');
-
-const addValue = document.querySelector('[data-action="decrement"]');
-addValue.addEventListener('click', () => {
-    counterValue -= 1; 
-    number.textContent = counterValue; 
-});
-const removeValue = document.querySelector('[data-action="increment"]');
-removeValue.addEventListener('click', ()=>{
-    counterValue += 1;
-    number.textContent = counterValue; 
-});*/
