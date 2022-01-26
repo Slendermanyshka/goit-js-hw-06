5b2e860c03fb9e65f409677bdb398369f84bd3e9
Slendermanyshka <slendermanyshka@gmail.com>
2022-01-26 15:20:04 +0200

    finished task 3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,11 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const gallery = document.querySelector('.gallery');
+const galEl = images.map((image) =>
+ `<li><img src="${image.url}" alt="${image.alt}" width="500" height="300"></li>`
+).join('');
+
+console.log(galEl);
+gallery.insertAdjacentHTML("beforeend",galEl);
